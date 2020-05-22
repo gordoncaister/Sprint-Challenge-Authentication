1b04d5ea124a61bd1aff4ba7c86b0d9d16c411b1
Gordon Caister <gordoncaister@gmail.com>
2020-05-22 10:43:08 -0500

    completed endpoints

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,11 +11,63 @@ router.get("/",(req,res)=> {
 
 router.post('/register', (req, res) => {
   // implement registration
+  const userDetails = req.body;
 
+    if (isValid(userDetails)){
+        const rounds = process.env.BCRYPT_ROUNDS || 4;
+        userDetails.password = bcryptjs.hashSync(userDetails.password, rounds);
+        console.log(userDetails)
+        Auth.add(userDetails)
+            .then(user=>{
+                console.log("user", user)
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({error: "Database error while registering"})
+            })
+    } else {
+        res.status(400).json({message:"Please provide valid username/password/department"})
+    }
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const {username, password} = req.body;
+    if(isValid(req.body)){
+        Auth.findBy({username})
+            .then(users => {
+
+                if (users[0] && bcryptjs.compareSync(password,users[0].password)){
+
+
+                    const token = generateToken(users[0])
+
+            
+
+                    res.status(200).json({message: "You made it!",token})
+                } else {
+                    res.status(401).json({message: "Your username/password is wrong or doesn't exist"})
+                }
+            })
+            .catch( err => res.status(500).json({error: "Database error while logging in"+err.message}))
+    } else {
+        res.status(400).json({message:"Please provide valid username/password"})
+    }
 });
+
+function isValid(user) {
+  return Boolean(user.username && user.password && typeof user.password === "string")
+}   
+
+function generateToken(user){
+  const payload = {
+      userId : user.id,
+      username : user.username
+  }
+  const secret = secrets.jwtSecret;
+  const options = {
+      expiresIn:"1d"
+  }
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,22 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+const secrets = require("./secrets");
+
+module.exports = (req,res,next)=>{
+    const token = req.headers.authorization;
+    const secret = secrets.jwtSecret;
+
+    if (token){
+
+        jwt.verify(token, secret, (error, decodedToken)=> {
+            if (error){
+                res.status(401).json({you: "cannot pass"})
+            } else {
+                req.decodedToken = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(400).json({message: "Please login or register."})
+    }
+}
